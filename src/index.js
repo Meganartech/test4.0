b24f8115fd86ec957fbb3561b4ab2e2da257141f
gowtham8420 <cheziangowtham@gmail.com>
2024-12-04 13:10:47 +0530

    tsest

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
-// import "swiper/css";
-// import "swiper/css/pagination";
-// import "swiper/css/navigation";
-// import "aos";n
-// import "aos/dist/aos.css";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
